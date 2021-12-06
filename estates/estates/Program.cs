ccd2121484c0305be83fda0bbf0c19b7a6bff2c9
marcinkaplon <95613707+marcinkaplon@users.noreply.github.com>
2021-12-06 11:06:18 +0100

    basic constructors added

--- estates/estates/Program.cs
+++ estates/estates/Program.cs
@@ -14,6 +14,15 @@ namespace estates
         string city;
         string phone_number;
         string estates_number;
+
+        protected Owner(string adress, string zip_code, string city, string phone_number, string estates_number)
+        {
+            this.adress = adress;
+            this.zip_code = zip_code;
+            this.city = city;
+            this.phone_number = phone_number;
+            this.estates_number = estates_number;
+        }
     }
 
     class Client
@@ -22,6 +31,14 @@ namespace estates
         string surname;
         string phone_number;
         DateTime date_of_birth;
+
+        public Client(string name, string surname, string phone_number, string data)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.phone_number = phone_number;
+            DateTime.TryParseExact(data, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date_of_birth);
+        }
     }
 
     class Employee
@@ -31,6 +48,15 @@ namespace estates
         string phone_number;
         decimal salary;
         decimal sale_bonus;
+
+        public Employee(string name, string surname, string phone_number, decimal salary, decimal sale_bonus)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.phone_number = phone_number;
+            this.salary = salary;
+            this.sale_bonus = sale_bonus;
+        }
     }
 
     public abstract class Estate
@@ -39,7 +65,7 @@ namespace estates
         string adress;
         string zip_code;
         string city;
-        double price;
+        decimal price;
         decimal area; //pol. powierzchnia
         bool furniture;
         bool balcony;
@@ -47,6 +73,36 @@ namespace estates
         string description;
         int bedrooms;
         Owner owner;
+
+        protected Estate(int id, string adress, string zip_code, string city, decimal price, decimal area, 
+            bool furniture, bool balcony, int rooms_numer, string description, int bedrooms, Owner owner)
+        {
+            this.id = id;
+            this.adress = adress;
+            this.zip_code = zip_code;
+            this.city = city;
+            this.price = price;
+            this.area = area;
+            this.furniture = furniture;
+            this.balcony = balcony;
+            this.rooms_numer = rooms_numer;
+            this.description = description;
+            this.bedrooms = bedrooms;
+            this.owner = owner;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Adress { get => adress; set => adress = value; }
+        public string Zip_code { get => zip_code; set => zip_code = value; }
+        public string City { get => city; set => city = value; }
+        public decimal Price { get => price; set => price = value; }
+        public decimal Area { get => area; set => area = value; }
+        public bool Furniture { get => furniture; set => furniture = value; }
+        public bool Balcony { get => balcony; set => balcony = value; }
+        public int Rooms_numer { get => rooms_numer; set => rooms_numer = value; }
+        public string Description { get => description; set => description = value; }
+        public int Bedrooms { get => bedrooms; set => bedrooms = value; }
+        public Owner Owner { get => owner; set => owner = value; }
     }
 
     class House:Estate
@@ -54,12 +110,31 @@ namespace estates
         int levels;
         bool garden;
         decimal garden_area;
+
+        public House(int id, string adress, string zip_code, string city, decimal price, 
+            decimal area, bool furniture, bool balcony, int rooms_number, string description,
+            int bedrooms, Owner owner, int levels, bool garden, decimal garden_area):base(id, adress, zip_code, city, price, area,
+                furniture, balcony, rooms_number, description, bedrooms, owner)
+        {
+            this.levels = levels;
+            this.garden = garden;
+            this.garden_area = garden_area;
+        }
     }
 
     class Flat : Estate
     {
         string building_development; //pol. zabudowa
         int level;
+
+        public Flat(int id, string adress, string zip_code, string city, decimal price,
+            decimal area, bool furniture, bool balcony, int rooms_number, string description,
+            int bedrooms, Owner owner, string building_development, int level):base(id, adress, zip_code, city, price, area,
+                furniture, balcony, rooms_number, description, bedrooms, owner)
+        {
+            this.building_development = building_development;
+            this.level = level;
+        }
     }
 
     class Meeting
@@ -69,24 +144,48 @@ namespace estates
         Estate estate;
         DateTime date;
         Kind_of_meeting kind;
+
+        public Meeting(Client client, Employee employee, Estate estate, string date1, Kind_of_meeting kind)
+        {
+            this.client = client;
+            this.employee = employee;
+            this.estate = estate;
+            DateTime.TryParseExact(date1, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date);
+            this.kind = kind;
+        }
     }
 
-    class PrivateOwner
+    class PrivateOwner:Owner
     {
         string name;
         string surname;
+
+        public PrivateOwner(string adress, string zip_code, string city, string phone_number, string estates_number, 
+            string name, string surname):base(adress, zip_code, city, phone_number, estates_number)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
     }
 
-    class Company
+    class Company:Owner
     {
         string NIP;
         string company_name;
+
+        public Company(string adress, string zip_code, string city, string phone_number, string estates_number,
+            string nip, string company_name): base(adress, zip_code, city, phone_number, estates_number)
+        {
+            NIP = nip;
+            this.company_name = company_name;
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            
 
         }
     }
