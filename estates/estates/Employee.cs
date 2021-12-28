39f14db01a41428ba642c502167afde75d7a7fc4
DawidKopec <dawidkopec@op.pl>
2021-12-28 22:48:29 +0100

    A few functions in EmployeesRepository
    add,remove,sort, and wlasciwosci in employees class

--- estates/estates/Employee.cs
+++ estates/estates/Employee.cs
@@ -2,7 +2,7 @@
 
 namespace estates
 {
-    class Employee
+    public class Employee
     {
         string _name;
         string _surname;
@@ -11,6 +11,14 @@ namespace estates
         decimal _sale_bonus;
         static decimal _amountForSale = 50;
         int _sold_estates = 0;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Surname { get => _surname; set => _surname = value; }
+        public string PhoneNumber1 { get => _phoneNumber; set => _phoneNumber = value; }
+        public decimal Salary { get => _salary; set => _salary = value; }
+        public decimal Sale_bonus { get => _sale_bonus; set => _sale_bonus = value; }
+        public static decimal AmountForSale { get => _amountForSale; set => _amountForSale = value; }
+        public int Sold_estates { get => _sold_estates; set => _sold_estates = value; }
 
         public Employee(string name, string surname, string phoneNumber, decimal salary, int sold_estates)
         {
--- estates/estates/EmployeesRepository.cs
+++ estates/estates/EmployeesRepository.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace estates
 {
-    class EmployeesRepository
+    class EmployeesRepository:IComparer<Employee>
     {
         string _name;
         List<Employee> _employelist;
 
+        public EmployeesRepository()
+        {
+            _employelist = new List<Employee>();
+        }
+        public EmployeesRepository(string n):this()
+        {
+            _name = n;
+        }
+        public void AddEmployee(Employee e)
+        {
+            _employelist.Add(e);
+        }
+        public void RemoveEmployee(Employee e)
+        {
+            _employelist.Remove(e);
+        }
+        public void EmploySort()
+        {
+            _employelist.Sort();
+        }
+
+        public int Compare(Employee x, Employee y)
+        {
+            if (x.Surname == y.Surname)
+                return x.Name.CompareTo(y.Name);
+            else
+                return x.Surname.CompareTo(y.Surname);
+        }
     }
 }
